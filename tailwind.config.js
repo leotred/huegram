a0eb4d398a1701fcb7f00d705e702aa75fcdb9c9
myschproj <150742169+myschproj@users.noreply.github.com>
2024-02-28 19:35:02 +0300

    Tемка не зашла :(

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        dusk: "#242526",
-        lightgray: "#3a3b3c",
-      },
-    },
+    extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms")({
-      strategy: "class",
-    }),
+    require('@tailwindcss/forms')({
+      strategy: 'class'
+    })
   ],
-  darkMode: "class",
-};
+}
